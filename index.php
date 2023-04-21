888db7b36be6323cb7e26e93e5b31ef71b257281
rohit1250rathod <rohitrathodse2024@gmail.com>
2023-04-21 18:59:08 +0530

    updates

---
+++ index.php
@@ -0,0 +1,111 @@
+<?php 
+  include "php/config.php";
+  $new_url = "";
+  if(isset($_GET)){
+    foreach($_GET as $key=>$val){
+      $u = mysqli_real_escape_string($conn, $key);
+      $new_url = str_replace('/', '', $u);
+    }
+      $sql = mysqli_query($conn, "SELECT full_url FROM url WHERE shorten_url = '{$new_url}'");
+      if(mysqli_num_rows($sql) > 0){
+        $sql2 = mysqli_query($conn, "UPDATE url SET clicks = clicks + 1 WHERE shorten_url = '{$new_url}'");
+        if($sql2){
+            $full_url = mysqli_fetch_assoc($sql);
+            header("Location:".$full_url['full_url']);
+          }
+      }
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>URL Shortener in PHP | CodingNepal</title>
+  <link rel="stylesheet" href="url.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head>
+<body>
+  <div class="header">
+    <h1>Url Shortener</h1>
+  </div>
+  <div class="wrapper">
+    <label for="" class="linkpath"><i class="fa-solid fa-link"></i>Enter Your Long URL to make a Short Url</label>
+    <form action="#" autocomplete="off">
+      <input type="text" spellcheck="false" name="full_url" placeholder="Enter or paste a long url" required>
+      <i class="url-icon uil uil-link"></i>
+      <button>Shorten</button>
+    </form>
+    <?php
+      $sql2 = mysqli_query($conn, "SELECT * FROM url ORDER BY id DESC");
+      if(mysqli_num_rows($sql2) > 0){;
+        ?>
+          <div class="statistics">
+            <?php
+              $sql3 = mysqli_query($conn, "SELECT COUNT(*) FROM url");
+              $res = mysqli_fetch_assoc($sql3);
+
+              $sql4 = mysqli_query($conn, "SELECT clicks FROM url");
+              $total = 0;
+              while($count = mysqli_fetch_assoc($sql4)){
+                $total = $count['clicks'] + $total;
+              }
+            ?>
+            <span>Total Links: <span><?php echo end($res) ?></span></span>
+            <a href="php/delete.php?delete=all">Clear All</a>
+        </div>
+        <div class="urls-area">
+          <div class="title">
+            <li>New URL</li>
+            <li>Original URL</li>
+            <li>delete</li>
+          </div>
+          <?php
+            while($row = mysqli_fetch_assoc($sql2)){
+              ?>
+                <div class="data">
+                <li>
+                  <a href="<?php echo $domain.$row['shorten_url'] ?>" target="_blank">
+                  <?php
+                    
+                      echo $domain.$row['shorten_url'];
+                    
+                  ?>
+                  </a>
+                </li> 
+                <li>
+                  <?php
+                    if(strlen($row['full_url']) > 60){
+                      echo substr($row['full_url'], 0, 60) . '...';
+                    }else{
+                      echo $row['full_url'];
+                    }
+                  ?>
+                </li> 
+              </li>
+                <li><a href="php/delete.php?id=<?php echo $row['shorten_url'] ?>"><i class="fa-solid fa-trash-can"></i></a></li>
+              </div>
+              <?php
+            }
+          ?>
+      </div>
+        <?php
+      }
+    ?>
+  </div>
+
+  <div class="blur-effect"></div>
+  <div class="popup-box">
+  <form action="#" autocomplete="off">
+    <label><i class="fa-solid fa-wand-magic-sparkles"></i>Customize your Link or Save</label>
+    <input type="text" class="shorten-url" spellcheck="false" required>
+    <i class="copy-icon uil uil-copy-alt"></i>
+    <button>Save</button>
+  </form>
+  </div>
+
+  <script src="script.js"></script>
+
+</body>
+</html>
+
---
+++ php/config.php
@@ -0,0 +1,12 @@
+<?php 
+
+    $domain = "localhost/url/";
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db = "urlShortener";
+    $conn = mysqli_connect($host, $user, $pass, $db);
+    if(!$conn){
+        echo "Database connection error".mysqli_connect_error();
+    }
+?>
